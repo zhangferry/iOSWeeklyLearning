33f960499f6daaa40df52b28cc0eb4d99c0b2aaf
zhangferry <457030189@qq.com>
2023-01-30 09:05:41 +0800

    docs: modify booknotes_3

--- Script/testScript.py
+++ Script/testScript.py
@@ -6,15 +6,27 @@ from content_category import export_category
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
+        pass
+
+    def test_deploy_article(self):
         article_args = ["--name", "campus_to_recruitment",
                         "--tags", "面试,职场,校园"]
         self.builder = BlogArticleBuilder(article_args)
 
-    def test_deploy_article(self):
         blog_branch = "master"
         blog_git_url = "git@github.com:zhangferry/GithubPage.git"
         blog_repo = BlogRepo(git_url=blog_git_url, branch=blog_branch)
         self.builder.run_with(blog_repo=blog_repo)
+
+    def test_deploy_book_notes(self):
+        article_args = ["--name", "cultivation_for_programmer_booknotes_1",
+                        "--tags", "书评,读书笔记,程序员的自我修养"]
+        builder = BlogArticleBuilder(article_args)
+
+        blog_branch = "master"
+        blog_git_url = "git@github.com:zhangferry/GithubPage.git"
+        blog_repo = BlogRepo(git_url=blog_git_url, branch=blog_branch)
+        builder.run_with(blog_repo=blog_repo)
 
     def test_export_category(self):
         # export_weekly_category()
